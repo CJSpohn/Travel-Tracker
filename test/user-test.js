da51a32c186505beae9014760eb1fefbad70cb14
Chris Spohn <chris.j.spohn@gmail.com>
2021-01-19 11:07:44 -0700

    Merge agent branch to test

---
+++ test/user-test.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+
+import User from '../src/User';
+import travelerData from 'travelers';
+
+let user1, user2, user3;
+
+
+describe('User', () => {
+
+  beforeEach(() => {
+    user1 = new User(travelerData[0]);
+    user2 = new User(travelerData[1]);
+    user3 = new User(travelerData[2]);
+  });
+
+  it('should have an id, name, travelerType, and trips', () => {
+    expect(user1).to.eql({
+      "id": 1,
+      "name": "Ham Leadbeater",
+      "travelerType": "relaxer"
+    });
+  })
+})
